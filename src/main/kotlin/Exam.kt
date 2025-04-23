ceabd34c89fc2834757d72dadefb7b9438cee2af
mad.lrmk.ru <admin@lrmk.ru>
2025-04-23 15:31:17 +0300

    first

---
+++ src/main/kotlin/Exam.kt
@@ -0,0 +1,22 @@
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.*
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import exam.*
+import kotlin.text.isEmpty
+import kotlin.text.toIntOrNull
+
+@Composable
+fun Exam() {
+
+}
